ed96393147fb0d62198c4b51f5af5d677287a5fb
J-1ac <tkdwns26@naver.com>
2024-09-23 02:59:30 +0900

    feat: 자기 정보 조회 로직 구현

--- be/withu/src/main/java/withu/member/service/MemberService.java
+++ be/withu/src/main/java/withu/member/service/MemberService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import withu.global.exception.CustomException;
 import withu.global.utils.JwtUtil;
+import withu.member.dto.MemberResponseDto;
 import withu.member.entity.Member;
 import withu.member.repository.MemberRepository;
 
@@ -14,10 +15,18 @@ import withu.member.repository.MemberRepository;
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    private final JwtUtil jwtUtil;
 
     public Member getMemberByEmail(String memberEmail) {
         return memberRepository.findByEmail(memberEmail)
             .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+    }
+
+    public MemberResponseDto getMember(Member member) {
+        return MemberResponseDto.builder()
+            .email(member.getEmail())
+            .name(member.getName())
+            .profile(member.getProfile())
+            .socialType(member.getSocialType())
+            .build();
     }
 }
