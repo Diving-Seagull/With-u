dcacf5bc9ae873c595aaf918ed0c5a472a824eb4
J-1ac <tkdwns26@naver.com>
2024-09-23 02:59:30 +0900

    feat: 자기 정보 조회 로직 구현

--- be/withu/src/main/java/withu/member/controller/MemberController.java
+++ be/withu/src/main/java/withu/member/controller/MemberController.java
@@ -1,7 +1,12 @@
 package withu.member.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import withu.auth.LoginMember;
+import withu.member.dto.MemberResponseDto;
+import withu.member.entity.Member;
 import withu.member.service.MemberService;
 
 @RestController
@@ -12,5 +17,10 @@ public class MemberController {
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping
+    public ResponseEntity<MemberResponseDto> getMember(@LoginMember Member member){
+        return ResponseEntity.ok(memberService.getMember(member));
     }
 }
