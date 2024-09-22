b010bc888dce0e631902285f257a5d7b146c7b10
J-1ac <tkdwns26@naver.com>
2024-09-22 12:58:13 +0900

    refactor: 프로젝트 명 변경

--- be/withu/src/main/java/boomerang/global/exception/AccessTokenNotExistsException.java
+++
@@ -1,9 +0,0 @@
-package boomerang.global.exception;
-
-import boomerang.global.response.ErrorCode;
-
-public class AccessTokenNotExistsException extends BusinessException {
-    public AccessTokenNotExistsException() {
-        super(ErrorCode.ACCESS_TOKEN_NOT_EXISTS_ERROR);
-    }
-}
