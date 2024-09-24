b25d36324818ec998d2c6f1ed5a255afe45e8052
J-1ac <tkdwns26@naver.com>
2024-09-24 14:41:47 +0900

    Apply .gitignore and remove ignored files from repository

---
+++ be/withu/src/main/java/withu/member/dto/MemberUpdateRequestDto.java
@@ -0,0 +1,9 @@
+package withu.member.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberUpdateRequestDto {
+    private String name;
+    private String profile;
+}
--- be/withu/src/main/java/withu/member/service/MemberService.java
+++ be/withu/src/main/java/withu/member/service/MemberService.java
@@ -2,11 +2,13 @@ package withu.member.service;
 
 import static withu.global.exception.ExceptionCode.USER_NOT_FOUND;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import withu.global.exception.CustomException;
 import withu.global.utils.JwtUtil;
 import withu.member.dto.MemberResponseDto;
+import withu.member.dto.MemberUpdateRequestDto;
 import withu.member.entity.Member;
 import withu.member.repository.MemberRepository;
 
@@ -33,5 +35,23 @@ public class MemberService {
     public MemberResponseDto getMemberByEmail(String memberEmail) {
         return MemberResponseDto.toDto(memberRepository.findByEmail(memberEmail)
             .orElseThrow(() -> new CustomException(USER_NOT_FOUND)));
+    }
+
+    @Transactional
+    public MemberResponseDto updateMemberInfo(Member member, MemberUpdateRequestDto updateRequestDto) {
+        // todo
+//        // 이름과 프로필 수정
+//        member.updateInfo(updateRequestDto.getName(), updateRequestDto.getProfile());
+//
+//        // 수정된 회원 정보 저장
+//        Member newMember = memberRepository.save(member);
+
+        // 수정된 정보를 DTO로 반환
+        return MemberResponseDto.builder()
+            .email(member.getEmail())
+            .name(member.getName())
+            .profile(member.getProfile())
+            .socialType(member.getSocialType())
+            .build();
     }
 }
