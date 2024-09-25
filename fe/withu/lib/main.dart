3ad3f7c74db403f66858a0f220a21e83c4a24b3b
JackAhn <an71195646@gmail.com>
2024-09-26 00:55:54 +0900

    Feature: Firebase 연동을 위한 패키지 설치 및 초기화 작업

--- fe/withu/lib/main.dart
+++ fe/withu/lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:withu/ui/page/splash_page.dart';
+import 'firebase_options.dart';
 
 void main() async{
   await dotenv.load(fileName: 'assets/config/.env'); // env 파일 로드
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); //Firebase 초기화
   WidgetsFlutterBinding.ensureInitialized(); // 프레임워크 초기화 여부 확인 (비동기 작업 시 수행)
   String? kakaoNativeAppKey = dotenv.env['KAKAO_APP_KEY'];
   KakaoSdk.init(nativeAppKey: kakaoNativeAppKey);
