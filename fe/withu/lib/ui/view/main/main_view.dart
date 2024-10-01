b4f253b37863a24003e0d9cf03521a0f175e826d
JackAhn <an71195646@gmail.com>
2024-10-01 14:45:06 +0900

    Feature: Bluetooth Advertisement 생성

--- fe/withu/lib/ui/view/main/main_view.dart
+++ fe/withu/lib/ui/view/main/main_view.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
+import 'package:uuid/uuid.dart';
 import 'package:withu/ui/page/main/home_page.dart';
 import 'package:withu/ui/view/empty_view.dart';
-import 'package:withu/ui/viewmodel/main/home_viewmodel.dart';
 
 import '../../../data/model/member.dart';
+import '../../global/device_info.dart';
 
 class MainView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    startAdvertisement();
     return CupertinoTabScaffold(
         tabBar: CupertinoTabBar(items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
@@ -30,5 +32,15 @@ class MainView extends StatelessWidget {
           }
         }
     );
+  }
+
+  // 팀원으로 로그인 시 Bluetooth Advertisement 활성화
+  void startAdvertisement() async {
+    try {
+      // 현재 로그인한 디바이스 정보를 네이티브 코드에 전달
+      await DeviceInfo.platform.invokeMethod('startAdvertising', {'deviceInfo': 'test' });
+    } on PlatformException catch (e) {
+      print("Failed to start advertising: '${e.message}'.");
+    }
   }
 }
