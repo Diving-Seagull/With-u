96351a7696c98856484c6a32eb5a7f8be8f7f0fc
JackAhn <an71195646@gmail.com>
2024-10-01 14:45:06 +0900

    Feature: Bluetooth Advertisement 생성

--- fe/withu/ios/Runner/AppDelegate.swift
+++ fe/withu/ios/Runner/AppDelegate.swift
@@ -1,13 +1,56 @@
-import Flutter
 import UIKit
+import Flutter
+import CoreBluetooth
 
-@main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+@UIApplicationMain
+class AppDelegate: FlutterAppDelegate, CBCentralManagerDelegate, CBPeripheralManagerDelegate {
+    var peripheralManager: CBPeripheralManager?
+    var advertisementData: [String: Any]?
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(name: "com.example.bluetooth/advertise",
+                                           binaryMessenger: controller.binaryMessenger)
+        
+        channel.setMethodCallHandler { (call, result) in
+            if call.method == "startAdvertising" {
+                if let args = call.arguments as? [String: Any],
+                   let teamInfo = args["teamInfo"] as? String {
+                    self.startAdvertising(with: teamInfo)
+                    result(nil)
+                } else {
+                    result(FlutterError(code: "INVALID_ARGUMENTS", message: "Expected team info", details: nil))
+                }
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        }
+
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    private func startAdvertising(with teamInfo: String) {
+        let teamInfoData = teamInfo.data(using: .utf8)!
+        let advertisementData = [
+            CBAdvertisementDataLocalNameKey: "WithuTeamDevice",
+            CBAdvertisementDataServiceUUIDsKey: [UUID().uuidString],
+            CBAdvertisementDataManufacturerDataKey: teamInfoData
+        ] as [String : Any]
+
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+        self.advertisementData = advertisementData
+    }
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        if peripheral.state == .poweredOn {
+            peripheralManager?.startAdvertising(advertisementData)
+        } else {
+            // Bluetooth is not available
+            print("Bluetooth is not available.")
+        }
+    }
 }
+
