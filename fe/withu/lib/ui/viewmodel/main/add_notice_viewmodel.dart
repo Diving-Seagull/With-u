469512e9fe1939181898e7f9c2512cac2fa6007b
JackAhn <an71195646@gmail.com>
2024-10-04 22:31:06 +0900

    Feature: 공지사항 작성 후 공지사항 데이터 갱신

--- fe/withu/lib/ui/viewmodel/main/add_notice_viewmodel.dart
+++ fe/withu/lib/ui/viewmodel/main/add_notice_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:withu/data/model/notice_request.dart';
 import 'package:withu/data/repository/notice_repository.dart';
 
@@ -6,6 +7,7 @@ import '../../../data/model/notice.dart';
 
 class AddNoticeViewModel with ChangeNotifier {
   final NoticeRepository _repository = NoticeRepository();
+  List<XFile>? pickedFiles;
 
   Future<Notice?> addNotice(NoticeRequest notice) async {
     return await _repository.addTeamNotice(notice);
