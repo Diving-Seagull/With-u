a0c99f3697f0a93efa3d1c1ea1a28babf36e4ce4
J-1ac <tkdwns26@naver.com>
2024-10-05 01:22:56 +0900

    feat: touristspot 엔티티 설계

---
+++ be/withu/src/main/java/withu/touristspot/entity/TouristSpot.java
@@ -0,0 +1,45 @@
+package withu.touristspot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class TouristSpot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Builder
+    private TouristSpot(Long id, String name, String address, String description, Double latitude, Double longitude) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
