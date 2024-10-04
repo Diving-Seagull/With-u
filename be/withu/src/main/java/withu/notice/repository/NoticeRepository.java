d9cca4d68e083781693878414e47dc3abd591601
J-1ac <tkdwns26@naver.com>
2024-10-05 07:49:16 +0900

    feat: 상단 고정된 공지사항 조회 기능 구현

--- be/withu/src/main/java/withu/notice/repository/NoticeRepository.java
+++ be/withu/src/main/java/withu/notice/repository/NoticeRepository.java
@@ -1,6 +1,7 @@
 package withu.notice.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import withu.notice.entity.Notice;
@@ -12,4 +13,6 @@ public interface NoticeRepository extends JpaRepository<Notice, Long> {
     List<Notice> findByTeamOrderByCreatedAtDesc(Team team);
 
     List<Notice> findByTeamAndPinnedTrue(Team team);
+
+    Optional<Notice> findFirstByTeamAndPinnedTrueOrderByCreatedAtDesc(Team team);
 }
