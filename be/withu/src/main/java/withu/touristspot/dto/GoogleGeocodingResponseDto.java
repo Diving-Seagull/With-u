d607e035ce091e9ab64b446bebd56bd6f6237e60
J-1ac <tkdwns26@naver.com>
2024-10-05 03:25:28 +0900

    refactor: 불필요한 setter, 로그 제거

--- be/withu/src/main/java/withu/touristspot/dto/GoogleGeocodingResponseDto.java
+++ be/withu/src/main/java/withu/touristspot/dto/GoogleGeocodingResponseDto.java
@@ -7,14 +7,12 @@ import lombok.Setter;
 import java.util.List;
 
 @Getter
-@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GoogleGeocodingResponseDto {
     private List<Result> results;
     private String status;
 
     @Getter
-    @Setter
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Result {
         private String formatted_address;
@@ -22,14 +20,12 @@ public class GoogleGeocodingResponseDto {
     }
 
     @Getter
-    @Setter
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Geometry {
         private Location location;
     }
 
     @Getter
-    @Setter
     public static class Location {
         private double lat;
         private double lng;
