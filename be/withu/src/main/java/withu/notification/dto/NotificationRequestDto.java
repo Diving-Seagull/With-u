564959bf14a8c0567e84bd0ba7a626582d031102
J-1ac <tkdwns26@naver.com>
2024-10-05 00:41:09 +0900

    feat: 팀 알림 전송 기능 추가

--- be/withu/src/main/java/withu/notification/dto/NotificationRequestDto.java
+++ be/withu/src/main/java/withu/notification/dto/NotificationRequestDto.java
@@ -9,10 +9,6 @@ import jakarta.validation.constraints.Positive;
 @Getter
 public class NotificationRequestDto {
 
-    @NotNull(message = "팀 ID는 필수입니다.")
-    @Positive(message = "팀 ID는 양수여야 합니다.")
-    private Long teamId;
-
     @NotNull(message = "대상 멤버 ID 리스트는 필수입니다.")
     @Size(min = 1, message = "최소 한 명 이상의 대상 멤버가 필요합니다.")
     private List<@NotNull @Positive(message = "멤버 ID는 양수여야 합니다.") Long> targetMemberIds;
