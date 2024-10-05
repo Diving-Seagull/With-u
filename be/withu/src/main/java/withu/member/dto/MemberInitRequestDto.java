453428febcec957ba61f296eba736199d2ef7e52
J-1ac <tkdwns26@naver.com>
2024-10-05 23:48:36 +0900

    feat: initMember시 languageCode validation 추가

--- be/withu/src/main/java/withu/member/dto/MemberInitRequestDto.java
+++ be/withu/src/main/java/withu/member/dto/MemberInitRequestDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import withu.global.validation.ValidLanguageCode;
 import withu.member.enums.Role;
 
 @Getter
@@ -28,6 +29,7 @@ public class MemberInitRequestDto {
 
     private Integer teamCode;
 
+    @ValidLanguageCode(message = "유효한 ISO 언어 코드를 입력해주세요.")
     @Size(min = 2, max = 10, message = "언어 코드는 2자 이상 10자 이하여야 합니다.")
     private String languageCode;
 }
