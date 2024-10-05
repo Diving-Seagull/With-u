46245ee26613b6eeaa57dcad3b38d7a73553da58
JackAhn <an71195646@gmail.com>
2024-10-05 19:30:32 +0900

    Feature: 삭제 시 사용자 확인 과정 추가

--- fe/withu/lib/ui/global/custom_dialog.dart
+++ fe/withu/lib/ui/global/custom_dialog.dart
@@ -12,4 +12,17 @@ class CustomDialog {
       );
     });
   }
+
+  static void showYesNoDialog(BuildContext context, String title, String message, Function() yesFunc, Function() noFunc) {
+    showCupertinoDialog(context: context, builder: (context) {
+      return CupertinoAlertDialog(
+        title: Text(title),
+        content: Text(message),
+        actions: [
+          CupertinoDialogAction(isDefaultAction: false, child: Text("취소"), onPressed: noFunc),
+          CupertinoDialogAction(isDefaultAction: true, child: Text("확인"), onPressed: yesFunc)
+        ],
+      );
+    });
+  }
 }
